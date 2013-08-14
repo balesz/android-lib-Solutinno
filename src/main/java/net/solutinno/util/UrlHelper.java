8ea5b4e553477fd793cbeba9d009e4600269bf61
Bode Balint <balint.bode@gmail.com>
2013-08-14 17:32:21 +0200

    Add getUrlFromString function to UrlHelper

--- src/main/java/net/solutinno/util/UrlHelper.java
+++ src/main/java/net/solutinno/util/UrlHelper.java
@@ -9,4 +9,9 @@ public class UrlHelper
     public static boolean isUrlValid(String url) {
         try { new URL(url); return Patterns.WEB_URL.matcher(url).matches(); } catch (Exception ex) { return false; }
     }
+
+    public static URL getUrlFromString(String url) {
+        if (!isUrlValid(url)) return null;
+        try { return new URL(url); } catch (Exception ex) { return null; }
+    }
 }
