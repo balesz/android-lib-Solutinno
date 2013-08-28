59e580c42b34f616b8d59cfb4eb33f48b72f797e
Bálint Böde <balint.bode@gmail.com>
2013-08-28 09:57:43 +0200

    Add CheckableLinearLayout and CheckableRelativeLayout

---
+++ src/main/java/net/solutinno/widget/CheckableRelativeLayout.java
@@ -0,0 +1,42 @@
+package net.solutinno.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Checkable;
+import android.widget.RelativeLayout;
+
+public class CheckableRelativeLayout extends RelativeLayout implements Checkable
+{
+    private boolean mChecked;
+
+    private static final int[] CHECKED_STATE_SET = { android.R.attr.state_checked };
+
+    public CheckableRelativeLayout(Context context) {
+        super(context);
+    }
+
+    public CheckableRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean isChecked() {
+        return mChecked;
+    }
+    @Override
+    public void toggle() {
+        setChecked(!mChecked);
+    }
+    @Override
+    public void setChecked(boolean checked) {
+        mChecked = checked;
+        refreshDrawableState();
+    }
+
+    @Override
+    protected int[] onCreateDrawableState(int extraSpace) {
+        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+        if (isChecked()) mergeDrawableStates(drawableState, CHECKED_STATE_SET);
+        return drawableState;
+    }
+}
