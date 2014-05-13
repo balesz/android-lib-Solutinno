0c628f4c455bad50357356d16c45fc213529b9fd
Bálint Böde <balint.bode@gmail.com>
2014-05-13 09:07:03 +0200

    Add getScreenRotation function to DisplayHelper

--- src/main/java/net/solutinno/util/DisplayHelper.java
+++ src/main/java/net/solutinno/util/DisplayHelper.java
@@ -1,17 +1,36 @@
 package net.solutinno.util;
 
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.util.DisplayMetrics;
 import android.view.Display;
+import android.view.Surface;
 import android.view.WindowManager;
 
 public class DisplayHelper
 {
     public static DisplayMetrics getDisplayMetrics(Context context) {
-        WindowManager mgr = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        Display defaultDisplay = mgr.getDefaultDisplay();
+        final Display display = ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
         DisplayMetrics result = new DisplayMetrics();
-        defaultDisplay.getMetrics(result);
+        display.getMetrics(result);
         return result;
+    }
+
+    public static int getScreenRotation(Context context) {
+
+        final Display display = ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+
+        switch (display.getRotation()) {
+            case Surface.ROTATION_0:
+                return ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+            case Surface.ROTATION_90:
+                return ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+            case Surface.ROTATION_180:
+                return ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+            case Surface.ROTATION_270:
+                return ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+            default:
+                return ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED;
+        }
     }
 }
